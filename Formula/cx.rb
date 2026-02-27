c07efdc302a745771f8beb3e005a685267d116a7
Jones <jsvitolo@gmail.com>
2026-02-27 08:29:09 -0300

    Update cx to v0.1.33

--- Formula/cx.rb
+++ Formula/cx.rb
@@ -1,16 +1,16 @@
 class Cx < Formula
   desc "AI-powered development platform with task management, semantic memory, and agent orchestration"
   homepage "https://github.com/jsvitolo/cortex-releases"
-  version "0.1.32"
+  version "0.1.33"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.32/cx_darwin_arm64.tar.gz"
-      sha256 "edf552d0ba151497f571c5601ca0e6812b83ff2da38a03ad2550fb3d21a59676"
+      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.33/cx_darwin_arm64.tar.gz"
+      sha256 "ded95ffe2438a6980fe142b2a9e08701975d43a138425b3e06621a2bfbd17215"
     else
-      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.32/cx_darwin_amd64.tar.gz"
-      sha256 "f5d272b6bd5346446c556297b251dc71a042334fdfc3c4c963b81970f5d44ff8"
+      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.33/cx_darwin_amd64.tar.gz"
+      sha256 "9cbff0a7534545c120118e7cf7ee187106238dd86ee152e8b677c9bf9e51fde0"
     end
   end
 
@@ -19,6 +19,6 @@ class Cx < Formula
   end
 
   test do
-    system "#{bin}/cx", "--version"
+    assert_match version.to_s, shell_output("#{bin}/cx --version")
   end
 end
