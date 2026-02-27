ed8e0329fcd2ea4f0fb267cd3c3271198ec297e3
Jones <jsvitolo@gmail.com>
2026-02-27 07:08:41 -0300

    chore: bump cx to v0.1.31

--- Formula/cx.rb
+++ Formula/cx.rb
@@ -1,16 +1,16 @@
 class Cx < Formula
   desc "AI-powered development platform with task management, semantic memory, and agent orchestration"
   homepage "https://github.com/jsvitolo/cortex-releases"
-  version "0.1.30"
+  version "0.1.31"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.30/cx_darwin_arm64.tar.gz"
-      sha256 "4c42cbd204bee147f4dbdeae102755f53f61d0012dcb786fdbb60ea3cee78d7f"
+      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.31/cx_darwin_arm64.tar.gz"
+      sha256 "63a539e1e3cd2d7374c0634a354e4c38a03bcc576e8615b50d982cde96835938"
     else
-      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.30/cx_darwin_amd64.tar.gz"
-      sha256 "25ff454dc5311fbdf4c98a5e527284b66edbb04637c29f835b76c864e0a71cc9"
+      url "https://github.com/jsvitolo/cortex-releases/releases/download/v0.1.31/cx_darwin_amd64.tar.gz"
+      sha256 "f44b5220b06d89000c083da5e11815fcaf56f6a5cdef04aa00f88c3039b2d1a9"
     end
   end
 
